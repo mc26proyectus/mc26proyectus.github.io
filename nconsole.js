80affc4a4ea2d4f9b554e218f81de2f4f3524e66
mc26proyectus <waloconelpotosucio@outlook.com>
2026-03-03 20:33:14 -0500

    Add files via upload

---
+++ nconsole.js
@@ -0,0 +1,80 @@
+document.getElementById('boybtnselect').addEventListener('click', function () {
+    const elementos0 = document.querySelectorAll('#atari78');
+
+    const elementos1 = document.querySelectorAll('#atarija');
+
+    const elementos11 = document.querySelectorAll('#atarilynx');
+
+    elementos0.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+
+    elementos1.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+
+    elementos11.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+});
+
+document.getElementById('btn78').addEventListener('click', function () {
+    const elementos2 = document.querySelectorAll('#atari26');
+
+    const elementos3 = document.querySelectorAll('#atarija');
+
+    const elementos31 = document.querySelectorAll('#atarilynx')
+
+    elementos2.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+
+    elementos3.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+
+    elementos31.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+});
+
+document.getElementById('btnja').addEventListener('click', function () {
+    const elementos4 = document.querySelectorAll('#atari26');
+
+    const elementos5 = document.querySelectorAll('#atari78');
+
+    const elementos6 = document.querySelectorAll('#atarilynx');
+
+    elementos4.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+
+    elementos5.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+
+    elementos6.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+});
+
+document.getElementById('btnlynx').addEventListener('click', function () {
+    const elementos7 = document.querySelectorAll('#atari26');
+
+    const elementos8 = document.querySelectorAll('#atari78');
+
+    const elementos9 = document.querySelectorAll('#atarija');
+
+    elementos7.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+
+    elementos8.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+
+    elementos9.forEach(function (elemento) {
+        elemento.style.display = 'none';
+    });
+});
+
